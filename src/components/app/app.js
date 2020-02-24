9b6b50bc55e60a4e12c9d9ec998f8374c24040cb
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-02-24 23:44:06 +0400

    activated search filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import AppHeader from '../app-header/index'
-import SearchPanel from '../search-panel/index'
-import TodoList from '../todo-list/index'
+import AppHeader from '../app-header/index';
+import SearchPanel from '../search-panel/index';
+import TodoList from '../todo-list/index';
 import ItemStatusFilter from '../item-status-filter/index';
-import ItemAddForm from "../item-add-form/index"
+import ItemAddForm from "../item-add-form/index";
 
 import './app.css';
 
@@ -19,7 +19,8 @@ export default class App extends Component {
             this.createTodoItem("Drink Coffee"),
             this.createTodoItem("Build Awesome App"),
             this.createTodoItem("Have a lunch"),
-        ]
+        ],
+        term: ''
     };
 
     // create Item 
@@ -30,7 +31,7 @@ export default class App extends Component {
             done: false,
             id: this.maxId++
         }
-    }
+    };
 
     deleteItem = (id) => {
         this.setState(({ todoData }) => {
@@ -71,14 +72,13 @@ export default class App extends Component {
             ...oldItem,
             [propName]: !oldItem[propName]
         };
-        
+
         return [
             ...arr.slice(0, idx), // elements in the left of deleted
             newItem,
             ...arr.slice(idx + 1), // elemetns in the right of deleted
         ]
-    }
-
+    };
     onToggleImportant = (id) => {
         this.setState(({ todoData }) => {
             return {
@@ -86,17 +86,31 @@ export default class App extends Component {
             };
         })
     };
-
     onToggleDone = (id) => {
         this.setState(({ todoData }) => {
             return {
                 todoData: this.toggleProperty(todoData, id, 'done')
             };
-        })
+        });
+    };
+
+    onSearchChange = (term) => {
+        this.setState({ term });
+    };
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
     };
 
     render() {
-        const { todoData } = this.state
+        const { todoData, term } = this.state;
+
+        const visibleItems = this.search(todoData, term);
 
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
@@ -105,11 +119,12 @@ export default class App extends Component {
             < div className="todo-app" >
                 <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
-                    <SearchPanel />
+                    <SearchPanel
+                        onSearchChange={this.onSearchChange} />
                     <ItemStatusFilter />
                 </div>
                 <TodoList
-                    todos={todoData}
+                    todos={visibleItems}
                     onDeleted={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleDone={this.onToggleDone} />
