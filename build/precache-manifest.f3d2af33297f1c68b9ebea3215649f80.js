fc508caf662ab4c0736794eb9851ab77c17194d7
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-03-03 15:12:35 +0400

    upgraded

---
+++ build/precache-manifest.f3d2af33297f1c68b9ebea3215649f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c931f236bd9ce02970ddd01d3647dcf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af52fbb1782266670cd",
+    "url": "/static/css/main.876a09c1.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af52fbb1782266670cd",
+    "url": "/static/js/main.ca5467d2.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
